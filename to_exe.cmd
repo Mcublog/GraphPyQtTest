c027a3123f7bd8da0cc7a304ba0765d20b36f23b
Vyacheslav Konovalov <konovalov@healbe.com>
2019-03-28 13:13:09 +0300

    added to_exe.cmd;

---
+++ to_exe.cmd
@@ -0,0 +1,2 @@
+pyinstaller --onefile main.py --paths %~dp0src --paths %~dp0ui --paths %~dp0ui\select_dialog --paths %~dp0ui\res
+REM pyinstaller --onefile main.py --paths %~dp0src --paths %~dp0ui --paths %~dp0ui\select_dialog --paths %~dp0ui\res
